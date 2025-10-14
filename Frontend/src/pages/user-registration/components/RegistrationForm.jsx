352c7e4f4d75b3bd353fe7941530d36b91700f43
anushmashine <anushmashine@gmail.com>
2025-10-14 18:48:21 +0530

    redirection fixes

--- Frontend/src/pages/user-registration/components/RegistrationForm.jsx
+++ Frontend/src/pages/user-registration/components/RegistrationForm.jsx
@@ -171,7 +171,7 @@ const RegistrationForm = ({ onSubmit, isLoading }) => {
       <div className="text-center">
         <p className="text-sm text-gray-600">
           Already have an account?{' '}
-          <Link to="/user-login" className="text-indigo-600 hover:underline font-medium">
+          <Link to="/login" className="text-indigo-600 hover:underline font-medium">
             Sign in here
           </Link>
         </p>
